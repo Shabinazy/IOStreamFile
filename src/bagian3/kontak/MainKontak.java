d2801e1d3ab32b55fe54327a358ef74399d281ef
Shabinazy <firdashabina29@gmail.com>
2026-06-24 21:49:11 +0800

    Create Main Kontak

--- src/bagian3/kontak/MainKontak.java
+++ src/bagian3/kontak/MainKontak.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package bagian3.kontak;
 
-/**
- *
- * @author User
- */
+
 public class MainKontak {
-    
+    public static void main(String[] args) {
+    // Membuat objek pengelola dan mengisinya
+    BukuKontak buku = new BukuKontak("kontak.txt");
+    buku.tambahKontak(new Kontak("Andi", "0811111"));
+    buku.tambahKontak(new Kontak("Budi", "0822222"));
+    buku.tambahKontak(new Kontak("Citra", "0833333"));
+    buku.tampilkanSemua();
+    buku.simpanKeBerkas();
+    System.out.println();
+    // Objek baru yang kosong, lalu memuat dari berkas
+    BukuKontak bukuLain = new BukuKontak("kontak.txt");
+    bukuLain.muatDariBerkas();
+    bukuLain.tampilkanSemua();
+    System.out.println("Jumlah kontak: " + bukuLain.jumlahKontak());
+    }
 }
